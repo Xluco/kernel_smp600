f56edd1283c8948c9659172475bc095960967c8a
halaszk <halaszk@deathlord.eu>
2014-04-06 20:22:52 +0200

    fixed gpio flip-cover

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -896,6 +896,7 @@ static void flip_cover_work(struct work_struct *work)
 	}
 }
 #else
+static struct input_dev *powerkey_device;
 static void flip_cover_work(struct work_struct *work)
 {
 	bool first;
@@ -913,7 +914,7 @@ static void flip_cover_work(struct work_struct *work)
 		input_report_switch(ddata->input,
 			SW_FLIP, ddata->flip_cover);
 		input_sync(ddata->input);
-	if (ddata->flip_cover == 0 && !suspended) {
+	if (ddata->flip_cover == 0) {
 		pr_info("%s: flip cover closed. Going to sleep ...\n", __func__);
 	        input_event(powerkey_device, EV_KEY, KEY_POWER, 1);
 	        input_event(powerkey_device, EV_SYN, 0, 0);
@@ -922,7 +923,7 @@ static void flip_cover_work(struct work_struct *work)
 	        input_event(powerkey_device, EV_KEY, KEY_POWER, 0);
 	        input_event(powerkey_device, EV_SYN, 0, 0);
 	}
-	if (ddata->flip_cover == 1 && suspended) {
+	if (ddata->flip_cover == 1) {
 		pr_info("%s: flip cover opened. Waking up ...\n", __func__);
 	        input_event(powerkey_device, EV_KEY, KEY_POWER, 1);
 	        input_event(powerkey_device, EV_SYN, 0, 0);
