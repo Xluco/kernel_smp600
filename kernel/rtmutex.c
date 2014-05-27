01dd5cc3cbb80c708f05333ef487260a1bd4d15d
Xluco <git@phugh.es>
2014-05-27 12:17:25 +0100

    various warnings & build fixes
    CRYPTO: Fix more AES build errors
    Building a multi-arch kernel results in:
    
    arch/arm/crypto/built-in.o: In function `aesbs_xts_decrypt':
    sha1_glue.c:(.text+0x15c8): undefined reference to `bsaes_xts_decrypt'
    arch/arm/crypto/built-in.o: In function `aesbs_xts_encrypt':
    sha1_glue.c:(.text+0x1664): undefined reference to `bsaes_xts_encrypt'
    arch/arm/crypto/built-in.o: In function `aesbs_ctr_encrypt':
    sha1_glue.c:(.text+0x184c): undefined reference to `bsaes_ctr32_encrypt_blocks'
    arch/arm/crypto/built-in.o: In function `aesbs_cbc_decrypt':
    sha1_glue.c:(.text+0x19b4): undefined reference to `bsaes_cbc_encrypt'
    
    This code is already runtime-conditional on NEON being supported, so
    there's no point compiling it out depending on the minimum build
    architecture.
    
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: LaboDJ <jacopolabardi@gmail.com>
    
    Fix for compiling error:
    
    	include/linux/kconfig.h:46:1: fatal error: drivers/gud//build_tag.h: No such file or directory
    		 #endif /* __LINUX_KCONFIG_H */
    
    various fixes from c0ryb4k3r

--- kernel/rtmutex.c
+++ kernel/rtmutex.c
@@ -723,7 +723,8 @@ rt_mutex_slowlock(struct rt_mutex *lock, int state,
 {
 	struct rt_mutex_waiter waiter;
 	int ret = 0;
-
+    
+    memset(&waiter, 0, sizeof(waiter));
 	debug_rt_mutex_init_waiter(&waiter);
 	RB_CLEAR_NODE(&waiter.pi_tree_entry);
 	RB_CLEAR_NODE(&waiter.tree_entry);
