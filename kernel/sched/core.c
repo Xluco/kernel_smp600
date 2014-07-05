c637411a11e08ccb003a383c2cbbb946532a4421
Xluco <git@phugh.es>
2014-07-05 16:20:29 +0100

    derp fixes IV: a derp hope

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1289,7 +1289,7 @@ ttwu_do_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
 
 	if (rq->idle_stamp) {
 		u64 delta = rq_clock(rq) - rq->idle_stamp;
-		u64 max = 2*rq->max_idle_balance_cost;
+		u64 max = 2*sysctl_sched_migration_cost;
 
 		//update_avg(&rq->avg_idle, delta);
 
