9637a86a7edb123a8161e435a5ac49f3761df11c
Xluco <dev@phugh.es>
2014-08-24 01:05:04 +0100

    Revert "sched: Add controls for sched features"
    This reverts commit f61c574f033312e4cb246e6c8e3c13f454083cb0.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1050,16 +1050,9 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 }
 
 static unsigned int Lgentle_fair_sleepers = 1;
-static unsigned int Larch_power = 1;
-
 void relay_gfs(unsigned int gfs)
 {
 	Lgentle_fair_sleepers = gfs;
-}
-
-void relay_ap(unsigned int ap)
-{
-	Larch_power = ap;
 }
 
 static void
@@ -3773,7 +3766,7 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 	struct sched_group *sdg = sd->groups;
 
 	if ((sd->flags & SD_SHARE_CPUPOWER) && weight > 1) {
-		if (Larch_power)
+		if (sched_feat(ARCH_POWER))
 			power *= arch_scale_smt_power(sd, cpu);
 		else
 			power *= default_scale_smt_power(sd, cpu);
@@ -3783,7 +3776,7 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 
 	sdg->sgp->power_orig = power;
 
-	if (Larch_power)
+	if (sched_feat(ARCH_POWER))
 		power *= arch_scale_freq_power(sd, cpu);
 	else
 		power *= default_scale_freq_power(sd, cpu);
