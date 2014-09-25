21b938c64c34f9e3ec598da319beebf0e6bc5fe5
Xluco <dev@phugh.es>
2014-09-25 17:42:44 +0100

    ARM: smp: Fix suspicious RCU originating from cpu_die()

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -211,7 +211,7 @@ void __ref cpu_die(void)
 	 * this returns, power and/or clocks can be removed at any point
 	 * from this CPU and its cache by platform_cpu_kill().
 	 */
-	complete(&cpu_died);
+	RCU_NONIDLE(complete(&cpu_died));
 
 	/*
 	 * Ensure that the cache lines associated with that completion are
